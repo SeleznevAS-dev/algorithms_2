df78307c2847a89a404e6dbbd6c3897abc4415bc
SeleznevAS-dev <its0a0magic0time@gmail.com>
2026-03-20 16:11:43 +0300

    Fix task 4 solution

--- tasks_and_tests/task_4/task_4.py
+++ tasks_and_tests/task_4/task_4.py
@@ -7,32 +7,36 @@ class aBST:
         tree_size = 2 ** (depth + 1)
         self.Tree: list[int | None] = [None] * tree_size  # массив ключей
 
-    def FindKeyIndexRecursive(self, current_index: int, key) -> int | None:
+    def FindKeyIndexRecursive(self, current_index: int, key: int) -> tuple[bool, int]:
         if current_index >= len(self.Tree) - 1:
-            return None
+            return False, -1
 
         tree_num = self.Tree[current_index]
-        if self.Tree[current_index] is None or self.Tree[current_index] == key:
-            return current_index
+        if tree_num is None:
+            return False, current_index
 
-        if tree_num > key:
+        elif tree_num == key:
+            return True, current_index
+
+        elif tree_num > key:
             next_index = (current_index * 2) + 1
             return self.FindKeyIndexRecursive(next_index, key)
         elif tree_num < key:
             next_index = (current_index * 2) + 2
             return self.FindKeyIndexRecursive(next_index, key)
 
+        return False, -1
+
     def FindKeyIndex(self, key) -> int | None:
+        is_found, index = self.FindKeyIndexRecursive(0, key)
         # ищем в массиве индекс ключа, None если не найден
-        return self.FindKeyIndexRecursive(0, key)
+        return index if is_found else None
 
     def AddKey(self, key) -> int:
-        found_key = self.FindKeyIndex(key)
-        if found_key is not None:
-            self.Tree[found_key] = key
-        else:
-            found_key = -1
-        return found_key
+        is_found, index = self.FindKeyIndexRecursive(0, key)
+        if not is_found and index != -1:
+            self.Tree[index] = key
+        return index
         # индекс добавленного/существующего ключа или -1 если не удалось
 
     def WideAllNodes(self) -> list[list]:
