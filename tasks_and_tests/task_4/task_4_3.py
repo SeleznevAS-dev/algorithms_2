e0d44bd69ca4280d5a387557f36fa41f5fabe88f
SeleznevAS-dev <its0a0magic0time@gmail.com>
2026-03-20 16:12:03 +0300

    Fix task 4 test

--- tasks_and_tests/task_4/task_4_3.py
+++ tasks_and_tests/task_4/task_4_3.py
@@ -15,6 +15,7 @@ def test_aBST_depth_0():
 def test_aBST_depth_1():
     tree = aBST(1)
     assert tree.AddKey(50) == 0
+    assert tree.FindKeyIndex(25) is None
     assert tree.AddKey(25) == 1
     assert tree.AddKey(75) == 2
 
@@ -86,3 +87,7 @@ def test_aBST_wide_all_nodes():
     assert wide_all_nodes[3][5] == 68
     assert wide_all_nodes[3][6] == 79
     assert wide_all_nodes[3][7] == 92
+
+# tree = aBST(1)
+# assert tree.AddKey(50) == 0
+# assert tree.FindKeyIndex(25) is None
